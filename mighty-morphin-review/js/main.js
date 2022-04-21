bcbbe00bed11ae0f065b825b956d57edafeb02bf
Kwame Obeng Yeboah <kwameobengyeboah@live.com>
2022-04-21 19:19:27 +0300

    updated solutions with ternary operators

--- mighty-morphin-review/js/main.js
+++ mighty-morphin-review/js/main.js
@@ -31,8 +31,18 @@ function compareThreeNumbers(arr1) {
     console.log(`Number of digits must be 3`);
   }
 }
-
 compareThreeNumbers([90, 15, 3, 7, 9]);
+
+//solution by Leon
+
+// function logHighAndLow(n1, n2, n3) {
+//   let minNum = Math.min(n1, n2, n3);
+//   console.log(minNum);
+//   let max = Math.max(n1, n2, n3);
+//   console.log(`The lowest num is ${minNum} and the highest num is ${max}`);
+// }
+
+// logHighAndLow([100, 50, 25]);
 
 // *Conditionals*
 //Create a function that returns heads or tails randomly and as fairly as possible. Call the function.
@@ -41,9 +51,9 @@ function randomHeadsOrTails() {
   let diceRoll = Math.floor(Math.random() * 2);
   console.log(diceRoll);
   if (diceRoll === 1) {
-    console.log(`heads`);
+    return "heads";
   } else if (diceRoll === 0) {
-    console.log("tail");
+    return "tails";
   }
 }
 
@@ -51,5 +61,20 @@ randomHeadsOrTails();
 randomHeadsOrTails();
 randomHeadsOrTails();
 
+//ternary solution
+
+const headsOrTails = (_) => (Math.random() < 0.5 ? "heads" : "tails");
+
+headsOrTails();
+
 //*Loops*
 //Create a function that takes in a number. Console log the result of heads or tails using the previous function x times where x is the number passed into the function. Call the function.
+
+function fliper(n) {
+  for (let i = 1; i <= n; i++) {
+    let result = randomHeadsOrTails();
+    console.log(result);
+  }
+}
+
+fliper(10);
