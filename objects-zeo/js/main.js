6941e919296b30c1e4b111645dcc6328a753aee2
Kwame Obeng Yeboah <kwameobengyeboah@live.com>
2022-04-22 22:52:51 +0300

    all sections solve

--- objects-zeo/js/main.js
+++ objects-zeo/js/main.js
@@ -1,1 +1,22 @@
 //Create a stopwatch object that has four properties and three methods
+
+let stopwatch = {};
+// properties literal declaration
+stopwatch.brand = "nike";
+stopwatch.color = "black";
+stopwatch.shape = "round";
+stopwatch.size = "medium";
+
+//object methods
+
+stopwatch.start = function () {
+  console.log("starting!!");
+};
+
+stopwatch.stop = function () {
+  console.log("stopping");
+};
+
+stopwatch.sayBrand = function () {
+  console.log(stopwatch.brand);
+};
